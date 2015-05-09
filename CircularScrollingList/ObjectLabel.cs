50a56dcaa49237672a3b74fe0fe69e97463df584
LanKuDot <lankudot@github.com>
2015-05-09 13:09:50 +0800

    Create ObjectLabel

---
+++ CircularScrollingList/ObjectLabel.cs
@@ -0,0 +1,15 @@
+﻿/* Make the gameObject follow another gameObject.
+ */
+using UnityEngine;
+using System.Collections;
+
+public class ObjectLabel : MonoBehaviour
+{
+	public Transform targetGameObject;
+	public Vector3 offset;
+
+	void Update()
+	{
+		transform.position = targetGameObject.position + offset;
+	}
+}
