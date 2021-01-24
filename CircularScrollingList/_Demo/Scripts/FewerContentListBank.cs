24d6bd718c71170a312214c38e84ff0f7c559b1a
LanKuDot <airlanser@gmail.com>
2021-01-24 14:19:32 +0800

    Create Demo directory to CircularScrollingList

---
+++ CircularScrollingList/_Demo/Scripts/FewerContentListBank.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class FewerContentListBank : BaseListBank
+{
+	private int[] contents = {
+		1, 2, 3,
+	};
+
+	public override string GetListContent(int index)
+	{
+		return contents[index].ToString();
+	}
+
+	public override int GetListLength()
+	{
+		return contents.Length;
+	}
+}
