80c07e884808dee7f9f08799883f54e5dad9a391
LanKuDot <airlanser@gmail.com>
2021-02-25 21:52:05 +0800

    Add FPSLimiter

---
+++ CircularScrollingList/_Demo/Scripts/FPSLimiter.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class FPSLimiter : MonoBehaviour
+{
+    [SerializeField]
+    private int _targetFPS = 60;
+
+    private void Start()
+    {
+        Application.targetFrameRate = _targetFPS;
+    }
+}
