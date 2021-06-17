4667ae822acdbbd5c960c6e23ee6a231f635f7ad
LanKuDot <airlanser@gmail.com>
2021-06-17 23:35:40 +0800

    ListBox: Custom list content displaying

---
+++ CircularScrollingList/_Demo/Scripts/MyListBox.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class MyListBox : ListBox
+{
+    [SerializeField]
+    private Text _contentText;
+
+    protected override void UpdateDisplayContent(object content)
+    {
+        _contentText.text = (string) content;
+    }
+}
