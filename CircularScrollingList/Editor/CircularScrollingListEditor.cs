3e030f033808516957312d1a53bde2226b4f713e
LanKuDot <airlanser@gmail.com>
2021-07-06 22:51:54 +0800

    Apply the custom editor for the CircularScrollingList

---
+++ CircularScrollingList/Editor/CircularScrollingListEditor.cs
@@ -0,0 +1,93 @@
+﻿using UnityEditor;
+
+namespace AirFishLab.ScrollingList.Editor
+{
+    [CustomEditor(typeof(CircularScrollingList))]
+    [CanEditMultipleObjects]
+    public class CircularScrollingListEditor : UnityEditor.Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            serializedObject.Update();
+
+            DrawPropertyField("_listBank");
+            DrawPropertyField("_listBoxes", true);
+            DrawSetting();
+
+            serializedObject.ApplyModifiedProperties();
+        }
+
+        private void DrawPropertyField(string path, bool includeChildren = false)
+        {
+            EditorGUILayout.PropertyField(
+                serializedObject.FindProperty(path), includeChildren);
+        }
+
+        #region Setting Property Drawer
+
+        private SerializedProperty _settingProperty;
+
+        private SerializedProperty GetSettingProperty(string path)
+        {
+            return _settingProperty.FindPropertyRelative(path);
+        }
+
+        private void DrawSettingProperty(string path)
+        {
+            EditorGUILayout.PropertyField(GetSettingProperty(path));
+        }
+
+        private void DrawSetting()
+        {
+            _settingProperty = serializedObject.FindProperty("_setting");
+            _settingProperty.isExpanded =
+                EditorGUILayout.Foldout(_settingProperty.isExpanded, "Setting");
+            if (!_settingProperty.isExpanded)
+                return;
+
+            ++EditorGUI.indentLevel;
+
+            DrawListMode();
+            EditorGUILayout.Space();
+            DrawListAppearance();
+            EditorGUILayout.Space();
+            DrawEvents();
+
+            --EditorGUI.indentLevel;
+        }
+
+        private void DrawListMode()
+        {
+            EditorGUILayout.LabelField("List Mode", EditorStyles.boldLabel);
+            DrawSettingProperty("_listType");
+            DrawSettingProperty("_controlMode");
+            var controlMode = GetSettingProperty("_controlMode");
+            if (controlMode.enumValueIndex
+                == (int) CircularScrollingList.ControlMode.Drag) {
+                ++EditorGUI.indentLevel;
+                DrawSettingProperty("_alignMiddle");
+                --EditorGUI.indentLevel;
+            }
+            DrawSettingProperty("_direction");
+            DrawSettingProperty("_centeredContentID");
+            DrawSettingProperty("_reverseOrder");
+        }
+
+        private void DrawListAppearance()
+        {
+            EditorGUILayout.LabelField("List Appearance", EditorStyles.boldLabel);
+            DrawSettingProperty("_boxDensity");
+            DrawSettingProperty("_boxPositionCurve");
+            DrawSettingProperty("_boxScaleCurve");
+            DrawSettingProperty("_boxMovementCurve");
+        }
+
+        private void DrawEvents()
+        {
+            EditorGUILayout.LabelField("Events", EditorStyles.boldLabel);
+            DrawSettingProperty("_onBoxClick");
+        }
+
+        #endregion
+    }
+}
