e28f30e32c910426fbbf34052e46c00da7cb3cab
LanKuDot <airlanser@gmail.com>
2021-07-15 23:51:36 +0800

    Create a CustomContentType demo

---
+++ CircularScrollingList/_Demo/Scripts/ColorStrListBank.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+
+namespace AirFishLab.ScrollingList.Demo
+{
+    public class ColorStrListBank : BaseListBank
+    {
+        [SerializeField]
+        private ColorString[] _contents;
+
+        public override object GetListContent(int index)
+        {
+            return _contents[index];
+        }
+
+        public override int GetListLength()
+        {
+            return _contents.Length;
+        }
+    }
+
+    [Serializable]
+    public class ColorString
+    {
+        public Color color;
+        public string name;
+    }
+}
---
+++ CircularScrollingList/_Demo/Scripts/ColorStrListBox.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace AirFishLab.ScrollingList.Demo
+{
+    public class ColorStrListBox : ListBox
+    {
+        [SerializeField]
+        private Image _contentImage;
+        [SerializeField]
+        private Text _contentText;
+
+        protected override void UpdateDisplayContent(object content)
+        {
+            var colorString = (ColorString) content;
+            _contentImage.color = colorString.color;
+            _contentText.text = colorString.name;
+        }
+    }
+}
