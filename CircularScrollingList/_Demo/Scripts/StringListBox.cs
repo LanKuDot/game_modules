da25d2790218fc79be3fa30c590642b424ad1928
LanKuDot <airlanser@gmail.com>
2021-07-30 23:09:44 +0800

    Add demo for refresh list

---
+++ CircularScrollingList/_Demo/Scripts/StringListBox.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace AirFishLab.ScrollingList.Demo
+{
+    public class StringListBox : ListBox
+    {
+        [SerializeField]
+        private Text _text;
+
+        protected override void UpdateDisplayContent(object content)
+        {
+            var dataWrapper = (VariableStringListBank.DataWrapper) content;
+            _text.text = dataWrapper.data;
+        }
+    }
+}
