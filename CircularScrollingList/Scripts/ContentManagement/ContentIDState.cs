c86d13fd2bd4744da2282321fe3b2d598d0250b8
LanKuDot <airlanser@gmail.com>
2022-11-27 15:13:43 +0800

    ContentIDState: Add NoContent state

--- CircularScrollingList/Scripts/ContentManagement/ContentIDState.cs
+++ CircularScrollingList/Scripts/ContentManagement/ContentIDState.cs
@@ -5,8 +5,21 @@
     /// </summary>
     public enum ContentIDState
     {
+        /// <summary>
+        /// The id is valid for getting the content
+        /// </summary>
         Valid,
+        /// <summary>
+        /// The id is less than 0
+        /// </summary>
         Underflow,
+        /// <summary>
+        /// The id is greater than or equals to the number of the contents
+        /// </summary>
         Overflow,
+        /// <summary>
+        /// There has no content in the list bank
+        /// </summary>
+        NoContent,
     }
 }
