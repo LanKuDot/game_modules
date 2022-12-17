78deeeb0998a6b65e994bc88d63487914a4f28f9
LanKuDot <airlanser@gmail.com>
2022-12-17 17:31:06 +0800

    ListBox: Remove old functions

--- CircularScrollingList/Scripts/ListBox.cs
+++ CircularScrollingList/Scripts/ListBox.cs
@@ -1,6 +1,5 @@
 ﻿using AirFishLab.ScrollingList.BoxTransformCtrl;
 using AirFishLab.ScrollingList.ContentManagement;
-using AirFishLab.ScrollingList.Util;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -35,10 +34,6 @@ namespace AirFishLab.ScrollingList
         /// </summary>
         public CircularScrollingList scrollingList { get; private set; }
         /// <summary>
-        /// The ID of this box in the registered boxes
-        /// </summary>
-        public int listBoxID { get; private set; }
-        /// <summary>
         /// The ID of the content that the box references
         /// </summary>
         public int contentID { get; private set; }
@@ -47,10 +42,7 @@ namespace AirFishLab.ScrollingList
 
         #region Referenced Components
 
-        private CircularScrollingListSetting _listSetting;
-        private ListPositionCtrl _positionCtrl;
         private ListContentManager _contentManager;
-        private ListBox[] _listBoxes;
 
         #endregion
 
@@ -92,28 +84,6 @@ namespace AirFishLab.ScrollingList
         #region Initialization
 
         /// <summary>
-        /// Initialize the box
-        /// </summary>
-        /// <param name="scrollingList">The list which this box belongs to</param>
-        /// <param name="listPositionCtrl">The position controller of this box</param>
-        /// <param name="listContentManager">The content controller</param>
-        /// <param name="listBoxID">The ID of this box</param>
-        public void Initialize(
-            CircularScrollingList scrollingList,
-            ListPositionCtrl listPositionCtrl,
-            ListContentManager listContentManager,
-            int listBoxID)
-        {
-            this.scrollingList = scrollingList;
-            this.listBoxID = listBoxID;
-
-            _listSetting = scrollingList.setting;
-            _positionCtrl = listPositionCtrl;
-            _contentManager = listContentManager;
-            _listBoxes = scrollingList.listBoxes;
-        }
-
-        /// <summary>
         /// Register the callback to the button clicking event
         /// </summary>
         private void RegisterClickEvent()
@@ -133,39 +103,6 @@ namespace AirFishLab.ScrollingList
         #region Content Handling
 
         /// <summary>
-        /// Disable the box if needed in the linear mode
-        /// </summary>
-        private void CheckToBeDisabled()
-        {
-            if (contentID == int.MinValue) {
-                gameObject.SetActive(false);
-                return;
-            }
-
-            if (_listSetting.listType != CircularScrollingList.ListType.Linear)
-                return;
-
-            // Disable the boxes at the upper half of the list
-            // which will hold the item at the tail of the contents.
-            if (contentID < 0) {
-                if (_listSetting.reverseOrder)
-                    _positionCtrl.numOfLowerDisabledBoxes += 1;
-                else
-                    _positionCtrl.numOfUpperDisabledBoxes += 1;
-                gameObject.SetActive(false);
-            }
-            // Disable the box at the lower half of the list
-            // which will hold the repeated item.
-            else if (contentID >= _contentManager.ContentCount) {
-                if (_listSetting.reverseOrder)
-                    _positionCtrl.numOfUpperDisabledBoxes += 1;
-                else
-                    _positionCtrl.numOfLowerDisabledBoxes += 1;
-                gameObject.SetActive(false);
-            }
-        }
-
-        /// <summary>
         /// Update the displaying content on the ListBox
         /// </summary>
         /// <param name="content">The content to be displayed</param>
@@ -180,37 +117,6 @@ namespace AirFishLab.ScrollingList
         private void UpdateDisplayContentPrivate()
         {
             UpdateDisplayContent(_contentManager.GetListContent(contentID));
-        }
-
-        /// <summary>
-        /// Recalculate the contentID and reacquire the content from the bank
-        /// </summary>
-        /// <param name="centerBoxID">The id of the centered box</param>
-        /// <param name="centerContentID">The content ID for the centered box</param>>
-        public void Refresh(int centerBoxID, int centerContentID)
-        {
-            var localPos = transform.localPosition;
-            var posFactor =
-                _listSetting.direction == CircularScrollingList.Direction.Horizontal
-                    ? FactorUtility.GetVector2X(localPos)
-                    : FactorUtility.GetVector2Y(localPos);
-            var tempBoxID = listBoxID;
-
-            // Make the box ID be "in order"
-            if (listBoxID > centerBoxID && posFactor > 0)
-                tempBoxID -= _listBoxes.Length;
-            else if (listBoxID < centerBoxID && posFactor < 0)
-                tempBoxID += _listBoxes.Length;
-
-            contentID =
-                _contentManager.GetContentID(tempBoxID - centerBoxID, centerContentID);
-
-            if (_contentManager.IsIDValid(contentID)) {
-                // Activate the previous inactivated box
-                gameObject.SetActive(true);
-                UpdateDisplayContentPrivate();
-            } else
-                CheckToBeDisabled();
         }
 
         #endregion
